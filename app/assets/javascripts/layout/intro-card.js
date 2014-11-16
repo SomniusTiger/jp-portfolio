58d09cd916afb92a35b8c018658d79e8af31922d
Eoin Thomas O'Hehir <mail@etohehir.com>
2014-11-16 00:04:33 -0500

    Each slide now takes up the full height of the page

--- app/assets/javascripts/layout/intro-card.js
+++ app/assets/javascripts/layout/intro-card.js
@@ -3,14 +3,16 @@
 
 $(function(){
   // Sets height on initial page load
-  $(".intro-card").css({
-    'line-height' : $(window).height() + "px"
+  $('.container').css({
+    'line-height' : $(window).height() + 'px',
+    'height' : $(window).height() + 'px'
   });
 
   // Sets height on window resize
   $(window).resize(function(){
-    $(".intro-card").css({
-      'line-height' : $(window).height() + "px"
+    $('.container').css({
+      'line-height' : $(window).height() + 'px',
+      'height' : $(window).height() + 'px'
     });
   });
 });

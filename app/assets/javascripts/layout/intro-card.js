0beec8a51444f4ba88a0233e3f28a75be03737ee
Eoin Thomas O'Hehir <mail@etohehir.com>
2014-11-16 00:53:29 -0500

    Proper animation timing, computer illustration size fix

--- app/assets/javascripts/layout/intro-card.js
+++ app/assets/javascripts/layout/intro-card.js
@@ -2,17 +2,29 @@
 // browser window height
 
 $(function(){
-  // Sets height on initial page load
+  setContainerHeight();
+  setComputerHeight();
+
+  // Sets height on window resize
+  $(window).resize(function(){
+    setContainerHeight();
+    setComputerHeight();
+  });
+
+});
+
+function setContainerHeight() {
   $('.container').css({
     'line-height' : $(window).height() + 'px',
     'height' : $(window).height() + 'px'
   });
+}
 
-  // Sets height on window resize
-  $(window).resize(function(){
-    $('.container').css({
-      'line-height' : $(window).height() + 'px',
-      'height' : $(window).height() + 'px'
-    });
+function setComputerHeight() {
+  $('.computer').css({
+    'max-height' : $(window).height() - 16 * 9 + 'px'
   });
-});
+  $('.computer .computer-img').css({
+    'height' : $(window).height() - 16 * 9 + 'px'
+  });
+}

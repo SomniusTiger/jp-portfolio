c80f4e5803a9ab3d47efb1a312d3afe43be83da6
Eoin Thomas O'Hehir <mail@etohehir.com>
2014-12-07 14:16:20 -0500

    Adjusted spacing on routes to be more legible

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   match via: :get, '/about' => 'public#about', as: :about
 
   # Project Pages
-  match via: :get,  '/bu-reel'        =>  'projects#bu-reel', as: :bu_reel
-  match via: :get,  '/dsb'            =>  'projects#dsb', as: :dsb
-  match via: :get,  '/illustrations'  =>  'projects#illustrations', as: :illustrations
-  match via: :get,  '/tap-of-war'     =>  'projects#tap-of-war', as: :tap_of_war
-  match via: :get,  '/vinyl-music'    =>  'projects#vinyl-music', as: :vinyl_music
+  match via: :get,  '/bu-reel'        =>  'projects#bu-reel',         as: :bu_reel
+  match via: :get,  '/dsb'            =>  'projects#dsb',             as: :dsb
+  match via: :get,  '/illustrations'  =>  'projects#illustrations',   as: :illustrations
+  match via: :get,  '/tap-of-war'     =>  'projects#tap-of-war',      as: :tap_of_war
+  match via: :get,  '/vinyl-music'    =>  'projects#vinyl-music',     as: :vinyl_music
 
 end

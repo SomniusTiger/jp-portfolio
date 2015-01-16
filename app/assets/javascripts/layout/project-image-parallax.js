6580b93c5a36dc19a4ac11c1ccf3f0e9066fd7d3
Eoin Thomas O'Hehir <mail@etohehir.com>
2015-01-15 23:59:28 -0500

    Mobile styles for projects complete, parallax edits for mobile

--- app/assets/javascripts/layout/project-image-parallax.js
+++ app/assets/javascripts/layout/project-image-parallax.js
@@ -3,14 +3,6 @@
 var parallaxScroll = new function() {
   var self = this;
 
-  this.imgSize = function() {
-    // Resize container element to always be the height of the page
-    var pageHeight =  $(window).height();
-    var pageWidth =   $(window).width();
-
-    $('.project-main-image-container').height(pageHeight);
-  };
-
   this.Scroll = function() {
     // As page scrolls, add negative top margin to the project image
     // relative to the amount the user scrolls
@@ -18,21 +10,20 @@ var parallaxScroll = new function() {
     // Get window scroll position
     var scrollTop = $(window).scrollTop();
 
-    // Set Y of translate3d to a third of the window's scroll position
-    // Scroll rate is also based on window height
-    $('.project-main-image').css({
-      'transform': 'translate3d(0, ' + Math.round( ( scrollTop / 3 ) * 10 ) / 10 + 'px, 0)'
-    });
+    // Get page width & height
+    var pageHeight =  window.innerHeight || window.documentElement.clientHeight || window.body.clientHeight;
+    var pageWidth =   window.innerWidth  || window.documentElement.clientWidth  || window.body.clientWidth;
+
+    // Move by third of the window's scroll position
+    // Don't scroll if less than a certain height, or not in landscape
+    if (pageWidth > 768 && pageWidth > pageHeight) {
+      $('.project-main-image').css({
+        'background-position': '50% ' + Math.round( ( -scrollTop / 3 ) * 10 ) / 10 + 'px',
+        'transform': 'translate3d(0, 0, 0)'
+      });
+    }
   };
 };
-
-$(function() {
-  parallaxScroll.imgSize();
-});
-
-$(window).resize(function() {
-  parallaxScroll.imgSize();
-});
 
 $(window).on('scroll', function() {
   parallaxScroll.Scroll();

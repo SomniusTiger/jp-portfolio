6c38da19bfe7bff415ef623f801bdbc16c72e37c
Eoin Thomas O'Hehir <eohehir@asheavenue.com>
2015-02-20 20:49:26 -0500

    Fuuuuuuck html5 video

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,9 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "video/mp4", :mp4
-Mime::Type.register "video/ogg", :ogv
-Mime::Type.register "video/webm", :webm
+# Mime::Type.register "video/mp4", :mp4
+# Mime::Type.register "video/ogg", :ogv
+# Mime::Type.register "video/webm", :webm
 
 Rack::Mime::MIME_TYPES.merge!({
   ".ogg"     => "application/ogg",

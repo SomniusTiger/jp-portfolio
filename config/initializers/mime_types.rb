b3b6b6f0813adfb04494e461b2ba5938e14cdde6
Eoin Thomas O'Hehir <eohehir@asheavenue.com>
2015-02-20 21:08:30 -0500

    Added html5 video through gfycat

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,19 +4,5 @@
 # Mime::Type.register "text/richtext", :rtf
 
 Mime::Type.register "video/mp4", :mp4
-MIME::Types.add(MIME::Type.from_array("video/mp4", %(mp4)))
 Mime::Type.register "video/ogg", :ogv
-MIME::Types.add(MIME::Type.from_array("video/ogg", %(ogv)))
 Mime::Type.register "video/webm", :webm
-MIME::Types.add(MIME::Type.from_array("video/webm", %(webm)))
-
-# Rack::Mime::MIME_TYPES.merge!({
-#   ".ogg"     => "application/ogg",
-#   ".ogx"     => "application/ogg",
-#   ".ogv"     => "video/ogg",
-#   ".oga"     => "audio/ogg",
-#   ".mp4"     => "video/mp4",
-#   ".m4v"     => "video/mp4",
-#   ".mp3"     => "audio/mpeg",
-#   ".m4a"     => "audio/mpeg"
-# })
